17bf538044c9a8de09aaeab4ac73f0f86e341eae
sadhu-sanjay <sanjaygoswami60@gmail.com>
2023-08-12 23:00:16 +0530

    working on fixed of state refresh

--- app/components/map-component.tsx
+++ app/components/map-component.tsx
@@ -7,6 +7,16 @@ import { MAPS_API_KEY } from "~/app/config";
 import { Spinner, Spinner2 } from "~/app/components/spinner";
 
 export function MapComponent() {
+  const render = (status: Status) => {
+    switch (status) {
+      case Status.LOADING:
+        return <Spinner />;
+      case Status.FAILURE:
+        return <div>Error Loading Div</div>;
+      case Status.SUCCESS:
+        return <MyMap {...mapOptions} records={records} />;
+    }
+  };
   const [records, setRecords] = useState<any[]>([]);
   const [selectedRecord, setSelectedRecord] = useState<any>(null);
 
@@ -22,30 +32,31 @@ export function MapComponent() {
       >
         {/* { selectedRecord && selectedRecord.id } */}
         <List records={records} setSelected={setSelectedRecord} />
-        <Wrapper apiKey={MAPS_API_KEY} render={render}>
-          <MyMap {...mapOptions}  />
-        </Wrapper>
+        <Wrapper apiKey={MAPS_API_KEY} render={render} />
       </div>
     </div>
   );
 }
 const mapOptions = {
-  center: { lat: -34.397, lng: 150.644 },
-  zoom: 8,
+  center: { lat: 38.50505275699189, lng: -98.9286968796735 },
+  zoom: 4,
 };
 
 function MyMap({
   center,
   zoom,
+  records,
 }: {
   center: google.maps.LatLngLiteral;
   zoom: number;
+  records?: any[];
 }) {
   const [map, setMap] = useState<google.maps.Map>();
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (ref.current) {
+      console.log("Setting REf");
       setMap(
         new window.google.maps.Map(ref.current, {
           center,
@@ -57,23 +68,17 @@ function MyMap({
 
   return (
     <>
-      <div ref={ref} className="w-full h-[96dvh] overflow-y-scroll " />;
+      <div ref={ref} className="w-full h-[96dvh] ">
+        {map &&
+          records?.map((record) => (
+            <MyMarker key={record.id} map={map} record={record} />
+          ))}
+      </div>
     </>
   );
 }
 
-const render = (status: Status) => {
-  switch (status) {
-    case Status.LOADING:
-      return <Spinner />;
-    case Status.FAILURE:
-      return <div>Error Loading Div</div>;
-    case Status.SUCCESS:
-      return <MyMap {...mapOptions} />;
-  }
-};
-
-function MyMarker({ record }: { record: any }) {
+function MyMarker({ record, map }: { record: any; map: google.maps.Map }) {
   var iconBase = "https://maps.google.com/mapfiles/kml/shapes/";
   const category: keyof typeof icons = record.fields.category;
 
@@ -84,7 +89,17 @@ function MyMarker({ record }: { record: any }) {
   };
   const icon = icons[category];
 
-  return <div> Marker </div>;
+  const Marker = new window.google.maps.Marker({
+    position: { lat: record.fields.lat, lng: record.fields.lng },
+    animation: window.google.maps.Animation.DROP,
+    icon: {
+      url: icon,
+      scaledSize: new window.google.maps.Size(32, 32),
+    },
+    map: map,
+  });
+
+  return <> Marker</>;
 }
 
 function List({
@@ -95,7 +110,7 @@ function List({
   setSelected: Dispatch<SetStateAction<any>>;
 }) {
   return (
-    <div className="w-1/2 p-6 h-[96dvh] overflow-y-scroll">
+    <div className="w-[40%] p-6 h-[96dvh] overflow-y-scroll">
       <h1 className="text-2xl font-bold">List of locations</h1>
       {records.length > 0 ? (
         <ul>
