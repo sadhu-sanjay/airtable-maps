d8105ca6edbae917529de39a12a8d466a2a44750
sadhu-sanjay <sanjaygoswami60@gmail.com>
2023-08-22 07:28:45 +0530

    working on implementing change

---
+++ app/components/dropdown.tsx
@@ -0,0 +1,9 @@
+export default function Dropdown() {
+  return (
+    <>
+      <div className="bg-red-100 w-full sm:max-w-xs h-full">
+        Sanjay Goswami
+      </div>
+    </>
+  );
+}
---
+++ app/components/sidebar.tsx
@@ -0,0 +1,12 @@
+import Dropdown from "./dropdown";
+
+export default function Sidebar() {
+  return (
+    <>
+      <div className="sidebar-container w-full h-full bg-red-300">
+        Sanjay goswami
+        {/* <Dropdown /> */}
+      </div>
+    </>
+  );
+}
