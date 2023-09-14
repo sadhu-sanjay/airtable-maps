21092a9aa0c22cf5e9a81a6ccc159835b446e8fa
sadhu-sanjay <sanjaygoswami60@gmail.com>
2023-09-14 10:24:32 +0530

    Added warning window

---
+++ app/components/common/alerts/warning-alert.tsx
@@ -0,0 +1,57 @@
+const WarningAlert = ({ text }: { text: string }) => {
+  return (
+    <div
+      id="alert-additional-content-4"
+      className="absolute self-center top-1/2  -translate-y-1/2 p-4 mb-4 text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 dark:border-yellow-800"
+      role="alert"
+    >
+      <div className="flex items-center">
+        <svg
+          className="flex-shrink-0 w-4 h-4 mr-2"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+        </svg>
+        <span className="sr-only">Info</span>
+        <h3 className="text-lg font-medium">Please wait.....</h3>
+      </div>
+      <div className="mt-2 mb-4 text-sm">{text}</div>
+      <div className="flex">
+        <button
+          onClick={() => window.location.reload()}
+          type="button"
+          className="text-white bg-yellow-800 hover:bg-yellow-900 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 text-center inline-flex items-center dark:bg-yellow-300 dark:text-gray-800 dark:hover:bg-yellow-400 dark:focus:ring-yellow-800"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-4 h-4 mr-1.5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+            />
+          </svg>
+          Reload
+        </button>
+        <button
+          type="button"
+          className="text-yellow-800 bg-transparent border border-yellow-800 hover:bg-yellow-900 hover:text-white focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-xs px-3 py-1.5 text-center dark:hover:bg-yellow-300 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-gray-800 dark:focus:ring-yellow-800"
+          data-dismiss-target="#alert-additional-content-4"
+          aria-label="Close"
+        >
+          Dismiss
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export { WarningAlert };
