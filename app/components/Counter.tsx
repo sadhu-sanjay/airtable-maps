232ccd476d139e4fdc1aee2c2708e94f6c95117a
sadhu-sanjay <sanjaygoswami60@gmail.com>
2023-09-15 16:36:28 +0530

    Added super scrolling

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -5,27 +5,32 @@ import { useEffect, useMemo } from "react";
 import { Spinner3, Spinner4 } from "./spinner";
 import useRecords from "./useRecords";
 import { WarningAlert } from "./common/alerts/warning-alert";
+import { FixedSizeList as List } from 'react-window';
 
 export default function Counter() {
   const { records } = useRecords();
 
-  // Use useMemo to prevent unnecessary rendering of records list
-  const recordsList = useMemo(() => {
-    return records.map((record) => (
-      <li className="text-sm " key={record.id}>
+  const Row = ({ index, style }) => {
+    const record = records[index];
+    return (
+      <li className="text-sm " key={record.id} style={style}>
         <strong>{record.sNo}.</strong> {record.Title}
       </li>
-    ));
-  }, [records]);
+    );
+  };
 
   return (
     <>
-      {/* {error &&  <WarningAlert text="Fetching records from airtable. Please try again in a minute...." />}
-      {loadingRecords && <Spinner3/>} */}
-      {/* {records.length === 0 && !loadingRecords && <div>No records found</div>} */}
       {records.length > 0 && (
         <div className="flex gap2 items-center">
-          <ul className="overflow-scroll h-screen">{recordsList}</ul>
+          <List
+            height={screen.height} // adjust this according to your needs
+            itemCount={records.length}
+            itemSize={30} // adjust this according to your needs
+            width='40%' // adjust this according to your needs
+          >
+            {Row}
+          </List>
           <div className="record-count">
             <strong>Record Count:</strong> {records.length}
           </div>
@@ -34,3 +39,4 @@ export default function Counter() {
     </>
   );
 }
+
