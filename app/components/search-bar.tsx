c5ec5b877c620aa1b8bca06e09b9fe352ed03211
sadhu-sanjay <sanjaygoswami60@gmail.com>
2023-10-05 19:29:26 +0530

    updated search bar

--- app/components/search-bar.tsx
+++ app/components/search-bar.tsx
@@ -23,7 +23,7 @@ export function SearchBar({
         Search
       </label>
       <div className="relative">
-        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
           <svg
             className="w-4 h-4 text-gray-500 dark:text-gray-400"
             aria-hidden="true"
