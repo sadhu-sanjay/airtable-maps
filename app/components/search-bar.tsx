44b5e20755abc5e1e02e5eb4e52af01f6fa2df36
sadhu-sanjay <sanjaygoswami60@gmail.com>
2023-10-05 19:28:26 +0530

    changed search bar

--- app/components/search-bar.tsx
+++ app/components/search-bar.tsx
@@ -53,25 +53,9 @@ export function SearchBar({
           required
         />
         {searchValue && (
-          <button
-            type="button"
-            className="absolute inset-y-0 right-0 flex items-center pr-3"
-            onClick={onClearButtonClick}
-          >
-            <svg
-              className="w-4 h-4 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M6 18L18 6M6 6l12 12"
-              />
+          <button type="button" className="absolute inset-y-0 right-0 flex items-center pr-3" onClick={onClearButtonClick} >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white" >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
           </button>
         )}
